1bd0c6e009c2d23da1a5ef809aa923f600dcd5c4
LIU ZHANWEI <50036800+Zhanwei-Liu@users.noreply.github.com>
2024-08-18 15:26:16 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -65,6 +65,24 @@ extensions = [
 
 #'sphinx.ext.mathjax', ??
 
+# VCS options: https://docs.readthedocs.io/en/latest/vcs.html#github
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "Zhanwei-Liu",  # Username
+    "github_repo": "spinningup",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "/docs/",  # Path in the checkout to the docs root
+    "spinningup_inject_language_links": True,
+    "spinningup_docs_supported_languages": list(supported_languages.keys()),
+    "spinningup_docs_basepath": "https://zhanwei-liu.github.io/spinningup/",
+    "spinningup_docs_suffix": ".html",
+    "spinningup_default_lang": "en",
+    "spinningup_canonical_version": "stable",
+    # Distinguish local development website from production website.
+    # This prevents people from looking for changes on the production website after making local changes :)
+    "spinningup_title_prefix": "" if on_rtd else "(DEV) ",
+}
+
 # imgmath settings
 imgmath_image_format = 'svg'
 imgmath_font_size = 14
